6248800d4f57ae90c281b57461e03b490e9970b2
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2020-02-01 14:07:46 +0500

    Start Javascrip

---
+++ class-8/app.js
@@ -0,0 +1,79 @@
+// var greeting = "Welcome";
+
+// console.log(greeting) // log
+// console.warn(greeting) // warning
+// console.error(greeting) // only for error
+
+
+// var greeting;
+// greeting = "Welcome";
+// var userName = "Majid"
+// console.log(greeting)
+
+
+// var num1 = 4;
+// num1 = 6;
+// var num2 = "4";
+// // console.log("Type of num1 ", typeof num1);
+// // // console.log("Type of num2 ", typeof num2);
+// console.log(num1 - num2)
+// console.log(num1 + num2)
+
+
+
+// var UserName
+// var userName
+// var User_Name
+// var user_name
+// var _userName
+// var USERNAME
+// var username
+// var username12
+
+// var 1userName
+// var -userName
+// var #userName
+// var @userName
+// var $userName
+// var user Name
+// var user/Name
+// var userAndName
+
+
+// var num1 = 4;
+// // ++num1;//5
+// // ++num1;//6
+// console.log(num1)
+// --num1;// 5
+// num1 = num1 + 6;
+// console.log(num1)
+
+// var greeting = "Hello ";
+// var userName = prompt("select a number between 1 to 5")
+
+
+
+// var num = 10;
+// if (10 == 10) {
+//     console.log("corrct number")
+// } else {
+//     console.log("Something went wrong")
+// }
+
+
+
+
+
+var ten = 10;
+var inputFromUser = +prompt("select a number between 1 to 5")
+if (isNaN(inputFromUser)) {
+    document.write("<h1>" + "Not A Number, Please provide correct number" + "</h1>")
+} else {
+    var sum = ten + inputFromUser
+    document.write("<h1>" + sum + "</h1>")
+}
+
+
+
+
+
